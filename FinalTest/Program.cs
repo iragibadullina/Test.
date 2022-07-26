7ac65fefd839226c377d50d0dc8a0b3f799e8c6c
IrinaGibadullina <gibadullina.irina@me.com>
2022-07-26 14:35:05 +0500

    Добавила решени задачи

--- FinalTest/Program.cs
+++ FinalTest/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿void MethodArray (string [] array)
+{
+     string[] newArray = new string[array.Length];
+     for (int i = 0; i < array.Length; i++)
+     {      
+        if(array[i].Length <= 3)
+        {
+        newArray[i] = array[i];;
+        Console.WriteLine(newArray[i]); 
+        }
+     }
+        Console.WriteLine(); 
+}
+
+void Print(string [] Array){
+    for (int i = 0; i < Array.Length; i++){
+        Console.WriteLine(Array[i]);
+    }
+    Console.WriteLine();
+}
+
+string [] array = {"hello", "2", "world", ":-)"};
+
+MethodArray(array);
+Print(array);
